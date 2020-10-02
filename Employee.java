bf315fd32cad7adbda8d0bc7155727aa32e4785a
vermavinay8948 <56758330+vermavinay8948@users.noreply.github.com>
2020-10-02 07:46:58 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -3,16 +3,20 @@ package com;
 public class Employee 
 {
 	//class variables class properties
-	int id;
-	String name;
-	String managerName;
-	double salary;
-	String comanyName;		
+	int id; // integer variable that stores id of customer
+	
+	String name;   //String variable that stores the name of customer
+	
+	String managerName;    //String variable that stores the name of manager
+	
+	double salary;     //double type variable that holds the salary of customer
+	
+	String comanyName;// string variable that holds the name of company		
 	
 	
-	double getBonus()
+	double getBonus()     //function which calculates the bonus over the salary
 	{
-		return salary * .1;
+		return salary * .1;    //bonus is 10 percent of salary
 	}
 	
 	
